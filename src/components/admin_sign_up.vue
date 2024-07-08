f553c1d5ce459589a463fbc595f9d1843da216d4
Kirill Petrikov <119692206+kiryasay@users.noreply.github.com>
2024-07-08 17:04:27 +0300

    Registration

--- src/components/admin_sign_up.vue
+++ src/components/admin_sign_up.vue
@@ -32,90 +32,141 @@ const submitForm = async () => {
 </script>
 
 <template>
+  <header>
+    <img src="../assets/Logo.svg" alt="">
+  </header>
   <div class="container">
-    <h1>Регистрация администратора</h1>
-    <form id="registrationform" @submit.prevent="submitForm">
-      <label for="login">Логин</label>
-      <input type="text" id="login" name="login" v-model="formData.login" required>
+    <div class = "form-background">
+      <h1>Добро пожаловать!</h1>
+      <form id="registrationform" @submit.prevent="submitForm">
+        <input type="text" id="login" name="login" v-model="formData.login" required placeholder="Логин">
 
-      <label for="email">Почта</label>
-      <input type="email" id="email" name="email" required>
+        <input type="email" id="email" name="email" required placeholder="Эл. почта">
 
-      <label for="password">Пароль</label>
-      <input type="password" id="password" name="password" v-model="formData.password" required>
+        <input type="password" id="password" name="password" v-model="formData.password" required placeholder="Пароль">
 
-      <button type="submit">Зарегистрироваться</button>
-    </form>
-    <p>Уже зарегистрированы? <a href="admin_sign_in.vue">Войти</a></p>
+        <button type="submit">Зарегистрироваться</button>
+      </form>
+      <p class="authorize">Уже зарегистрированы? <a href="admin_sign_in.vue">Войти</a></p>
+    </div>
   </div>
 </template>
 
 <style scoped>
 body {
-  font-family: Arial, sans-serif;
-  background-color: #f9f9f9;
+  background-color: rgb(255, 255, 255);
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
 
 header {
+  /* Logo */
+  position: absolute;
+  width: 291px;
+  height: 100px;
+  left: 42px;
+  top: 10px;
+}
+
+.container {
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
-  margin-bottom: 1rem;
-}
-
-h1 {
-  margin: 0;
-  font-size: 1.5rem;
-}
-
-.logout {
-  text-decoration: none;
-  color: #007bff;
-  font-weight: bold;
-}
-
-.status-filters {
-  display: flex;
-  justify-content: space-around;
-  margin-bottom: 1rem;
-}
-
-.status-button {
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  cursor: pointer;
-  background-color: #f0f0f0;
-}
-
-table {
+  height: 100%;
   width: 100%;
-  border-collapse: collapse;
 }
 
-th, td {
-  padding: 0.5rem;
-  text-align: center;
-  border: 1px solid #ccc;
-}
-
-.approve-button, .reject-button {
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
-  border: none;
+.form-background {
+  width: 450px;
+  height: 450px;
+  padding: 30px;
   border-radius: 5px;
-  cursor: pointer;
+  box-shadow: 4px 4px 4px 0px rgb(0, 0, 0, 0.5);
+  background: rgb(255, 255, 255);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 10%; 
+}
+h1{
+  color: rgb(63, 85, 101);
+  font-family:"Inter-regular";
+  font-size: 32px;
+  font-weight: 400;
+  line-height: 39px;
+  letter-spacing: 0%;
+  text-align: left;
+  padding-bottom: 20px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
 }
 
-.approve-button {
-  background-color: #28a745;
-  color: #fff;
+input {
+  width: 380px;
+  height: 25px;
+  padding: 10px;
+  margin: 10px 0;
+  border-radius: 5px;
+  border: 1px solid rgb(63, 85, 101);
+
+  font-family: "Inter-regular";
+  font-size:20px;
 }
 
-.reject-button {
-  background-color: #dc3545;
-  color: #fff;
-}
+button {
+  padding: 10px;
+  margin: 40px 20px 10px 20px;
+  width: 400px;
+  background-color:  rgb(63, 85, 101);
+  text-decoration: none;
+  border-radius: 5px;
+  border:none;
+  outline: none;
 
+  color:white;
+  font-family: "Inter-regular";
+  font-size: 20px;
+  font-weight: 100;
+  line-height: 29px;
+  letter-spacing: 0%;
+  text-align: center;
+}
+button:focus{
+  outline: none;
+}
+button:hover{
+  background-color:  rgb(134, 166, 189);
+  transition: all 0.5s ease;
+}
+input:focus{
+  outline: none;
+}
+::placeholder{
+  font-family: "Inter-regular";
+  font-size: 20px;
+  color: rgb(63, 85, 101);
+}
+.authorize{
+  padding-top: 20px;
+  font-family: "Inter-light";
+  font-size: 20px;
+  font-weight: 400px;
+  color: rgb(63, 85, 101);;
+}
+@font-face {
+  font-family: "Inter-regular";
+  src: url(/src/fonts/Inter-Regular.ttf);
+}
+@font-face {
+  font-family: "Inter-light";
+  src: url(/src/fonts/Inter-Light.ttf);
+}
 </style>
