73b1cc7abcf1826f68e355fab788d7c49e3ee999
Kirill Petrikov <119692206+kiryasay@users.noreply.github.com>
2024-07-08 21:18:49 +0300

    aaa

--- src/components/approve.vue
+++ src/components/approve.vue
@@ -4,14 +4,25 @@ import galochkaImage from '@/assets/galochka.png';
 </script>
 
 <template>
+  <header>
+    <img src="../assets/Logo.svg" alt="" class = "logo">
+  </header>
   <div class="container">
     <h1>Данные успешно отправлены!</h1>
     <p>Скоро на указанную Вами почту будет отправлен QR-код, который вам нужно будет предъявить для верификации на мероприятии.</p>
-    <img :src="galochkaImage" alt="Галочка">
+    <img src="../assets/check-circle.svg" alt="" class = "cirlce">
     <h1>Ждем Вас на мероприятии!</h1>
   </div>
 </template>
 
 <style scoped>
-
+.container{
+  display:-moz-groupbox;
+  align-items: center;
+  justify-content: center;
+}
+.cirlce{
+  width: 300px;
+  height: 300px;
+}
 </style>
--- src/components/registration_form.vue
+++ src/components/registration_form.vue
@@ -3,103 +3,159 @@
 </script>
 
 <template>
+  <header>
+    <img src="../assets/Logo.svg" alt="">
+  </header>
   <div class="container">
-    <h1>Заполните данные</h1>
-    <p>Проверьте корректность введённых данных, так как по ним будет проходить верификация на мероприятии.</p>
-    <form id="registrationForm" method="POST" action="http://localhost:3000/your-server-endpoint">
-      <label for="firstName">Имя</label>
-      <input type="text" id="firstName" name="firstName" placeholder="Ваше полное имя" required>
+    <div class = form-background >
+      <h1>Регистрация на: </h1>
 
-      <label for="middleName">Фамилия</label>
-      <input type="text" id="middleName" name="middleName" placeholder="Ваша фамилия">
+      <form id="registrationForm" method="POST" action="http://localhost:3000/your-server-endpoint">
+        <input type="text" id="middleName" name="middleName" placeholder="Фамилия" required>
 
-      <label for="lastName">Отчество</label>
-      <input type="text" id="lastName" name="lastName" placeholder="Ваше отчество (при наличии)" required>
+        <input type="text" id="firstName" name="firstName" placeholder="Имя" required>
 
-      <label for="company">Компания</label>
-      <input type="text" id="company" name="company" placeholder="Полное наименование компании" required>
+        <input type="text" id="lastName" name="lastName" placeholder="Отчество (при наличии)">
 
-      <label for="position">Должность</label>
-      <input type="text" id="position" name="position" placeholder="Ваша должность" required>
+        <input type="text" id="company" name="company" placeholder="Полное наименование компании" required>
 
-      <label for="email">Email</label>
-      <input type="email" id="email" name="email" placeholder="Корпоративный адрес" required>
+        <input type="text" id="position" name="position" placeholder="Должность" required>
 
-      <label for="phone">Контактный телефон</label>
-      <input type="tel" id="phone" name="phone" placeholder="Ваш номер телефона" required>
+        <input type="email" id="email" name="email" placeholder="Корпоративный адрес" required>
 
-      <button type="submit">Отправить</button>
-    </form>
-    <p>Нажимая кнопку «Отправить», Вы соглашаетесь с Политикой конфиденциальности и условиями обслуживания.</p>
+        <input type="tel" id="phone" name="phone" placeholder="Номер телефона" required>
+
+        <button type="submit">Отправить</button>
+      </form>
+      <p class = "bottom-text" >Проверьте корректность введённых данных, так как по ним будет проходить верификация на мероприятии.</p>
+      <p class = "bottom-text">Нажимая кнопку «Отправить», Вы соглашаетесь с Политикой конфиденциальности и условиями обслуживания.</p>
+    </div>
   </div>
 </template>
 
 <style scoped>
 body {
-  font-family: Arial, sans-serif;
-  background-color: #f9f9f9;
+  background-color: rgb(255, 255, 255);
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
 
 header {
+  /* Logo */
+  position: absolute;
+  width: 291px;
+  height: 100px;
+  left: 42px;
+  top: 10px;
+}
+
+.container {
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
-  margin-bottom: 1rem;
-}
-
-h1 {
-  margin: 0;
-  font-size: 1.5rem;
-}
-
-.logout {
-  text-decoration: none;
-  color: #007bff;
-  font-weight: bold;
-}
-
-.status-filters {
-  display: flex;
-  justify-content: space-around;
-  margin-bottom: 1rem;
-}
-
-.status-button {
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  cursor: pointer;
-  background-color: #f0f0f0;
-}
-
-table {
+  height: 100%;
   width: 100%;
-  border-collapse: collapse;
 }
 
-th, td {
-  padding: 0.5rem;
-  text-align: center;
-  border: 1px solid #ccc;
-}
-
-.approve-button, .reject-button {
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
-  border: none;
+.form-background {
+  width: 800px;
+  height: 800px;
+  padding: 30px;
   border-radius: 5px;
-  cursor: pointer;
+  background: rgb(255, 255, 255);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 3%; 
+}
+h1{
+  color: rgb(63, 85, 101);
+  font-family:"Inter-regular";
+  font-size: 32px;
+  font-weight: 400;
+  line-height: 39px;
+  letter-spacing: 0%;
+  text-align: left;
+  padding-bottom: 5px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
 }
 
-.approve-button {
-  background-color: #28a745;
-  color: #fff;
+input {
+  width: 380px;
+  height: 25px;
+  padding: 10px;
+  margin: 10px 0;
+  border-radius: 5px;
+  border: 1px solid rgb(63, 85, 101);
+
+  font-family: "Inter-regular";
+  font-size:20px;
 }
 
-.reject-button {
-  background-color: #dc3545;
-  color: #fff;
-}
+button {
+  padding: 10px;
+  margin: 10px 20px 10px 20px;
+  width: 400px;
+  background-color:  rgb(63, 85, 101);
+  text-decoration: none;
+  border-radius: 5px;
+  border:none;
+  outline: none;
 
+  color:white;
+  font-family: "Inter-regular";
+  font-size: 20px;
+  font-weight: 100;
+  line-height: 29px;
+  letter-spacing: 0%;
+  text-align: center;
+}
+button:focus{
+  outline: none;
+}
+button:hover{
+  background-color:  rgb(134, 166, 189);
+  transition: all 0.5s ease;
+}
+input:focus{
+  outline: none;
+}
+::placeholder{
+  font-family: "Inter-regular";
+  font-size: 20px;
+  color: rgb(63, 85, 101);
+}
+.authorize{
+  padding-top: 20px;
+  font-family: "Inter-light";
+  font-size: 20px;
+  font-weight: 400px;
+  color: rgb(63, 85, 101);;
+}
+.bottom-text{
+  display: flex;
+  font-family: "Inter-regular";
+  align-items: center;
+  color: black;
+  font-size: 18px;
+  text-align: center;
+  margin: 0;
+}
+@font-face {
+  font-family: "Inter-regular";
+  src: url(/src/fonts/Inter-Regular.ttf);
+}
+@font-face {
+  font-family: "Inter-light";
+  src: url(/src/fonts/Inter-Light.ttf);
+}
 </style>
