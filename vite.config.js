a6595ff82c203bd92c3c174ec135d8444876b26e
Takeasoul <odenter2003@gmail.com>
2024-07-13 10:28:09 +0300

    t

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  base: '/event-iac/',
+  base: '/',  // Use '/' as base path for render.com deployment
   plugins: [vue()],
   resolve: {
     alias: {
